834e1b937f670a7f03a109010ccb10e0237ea04c
Kieran Quinn <L00049485@student.lyit.ie>
2018-06-16 13:42:28 +0100

    Work on setting up entity framework test

---
+++ MovieReview/MovieReview/API/Controller/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieReview.API;
+
+namespace MovieReview.API.Controller
+{
+    public class MovieController
+    {
+        public void TestSp()
+        {
+            List<spTest_Result> results = null;
+
+            using (MovieDbEntities dbContext)
+            {
+
+            }
+        }
+    }
+}
