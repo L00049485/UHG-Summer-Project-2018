d82e844e56b2d319b8287b7699847c19e436e93d
Kieran Quinn <L00049485@student.lyit.ie>
2018-06-19 20:38:27 +0100

    Entity framework - work

---
+++ MovieReviewNew/MovieReviewNew/API/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿namespace MovieReviewNew.API.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using MovieReviewNew.API.Models;
+    using Newtonsoft.Json;
+
+    [RoutePrefix("api/Data")]
+    public class DefaultController : ApiController
+    {
+        [HttpPost]
+        [Route("TestSp")]
+        public IHttpActionResult TestStp()
+        {
+            List<spTest_Result> results = null;
+            try
+            {
+                using (EFTestdbEntities dbContext = new EFTestdbEntities())
+                {
+                    results = new List<spTest_Result>();
+                    results = dbContext.spTest();
+                }
+
+                return Ok(JsonConvert.SerializeObject(results, Formatting.Indented));
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
