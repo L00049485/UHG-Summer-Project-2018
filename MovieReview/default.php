8f8eee40a325dfb9540fe0ee51ab3575667906eb
Kieran Quinn <L00049485@student.lyit.ie>
2018-07-07 14:51:24 +0100

    Setting up files for code.

--- MovieReview/default.php
+++ MovieReview/default.php
@@ -1,13 +1,12 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-    <title>Axe Property</title>
+    <title>Movie Review</title>
     <meta charset="utf-8" />
 
     <!-- Scripts -->
     <script src="scripts/jQuery_3.3.1.js"></script>
     <script src="scripts/bootstrap.js"></script>
-    <script src="scripts/Custom.js"></script>
 
     <!-- Styles -->
     <link href="styles/mdb.css" rel="stylesheet" />
@@ -17,35 +16,17 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,500,600,700" rel="stylesheet">
 
-    <!-- noUISlider -->
-    <link href="scripts/noUiSlider/nouislider.css" rel="stylesheet" />
-    <script src="scripts/noUiSlider/nouislider.js"></script>
-    <script src="scripts/noUiSlider/wNumb.js"></script>
-
     <!--Chosen-->
     <link href="scripts/Chosen/chosen.css" rel="stylesheet" />
     <script src="scripts/Chosen/chosen.jquery.js"></script>
     <script src="scripts/Chosen/chosen.proto.js"></script>
 </head>
 <body>
-    <?php include("includes/header.html");?>
-    <br />
-    <br />
-    <br />
+
     <div id="content">
-        <div id="header-bread">
-            <ul class="breadcrumbs">
-                <li class="">Home</li>
-            </ul>
-        </div>
-		<?php include("includes/featured.php");?>
+        
 
     </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <?php include("includes/footer.html");?>
 
     <script src="scripts/mdb.js"></script>
 </body>
